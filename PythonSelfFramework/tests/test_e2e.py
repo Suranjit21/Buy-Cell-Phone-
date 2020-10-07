edce22e6d94bc062189a1dc138375d8aec421daf
suranjit mohajan <suranjitmohajan@gmail.com>
2020-10-07 13:27:00 -0400

    first commit to cell phone purchase project

---
+++ PythonSelfFramework/tests/test_e2e.py
@@ -0,0 +1,43 @@
+from PythonSelfFramework.pageObjects.homepage import Homepage
+from PythonSelfFramework.utilities.BaseClass import BaseClass
+
+
+# @pytest.mark.usefixtures("ourSetup")
+class TestTwo(BaseClass):
+
+   def tests_e2e(self):
+
+       home = Homepage(self.driver)
+       home.clickshopbutton().click()
+       cards = home.cardscountlist()
+
+
+
+
+
+       i = -1
+       for card in cards:
+           i = i + 1
+           cardText = card.text
+
+           if cardText == "Blackberry":
+
+                home.addblackberry().click()
+
+
+
+
+       checkoutpage = home.clickcheckout()
+       confirmpage = checkoutpage.clickcheckoutbutton()
+
+       confirmpage.searchbar().send_keys("ind")
+
+       self.waitmethod("India")
+
+       confirmpage.confirmcountry().click()
+
+       confirmpage.agreecheckbox().click()
+
+       confirmpage.purchase().click()
+
+
